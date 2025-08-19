8106b1ad9fc0b69fb25cf373c684052d18ab0823
mewmaze <aybin0108@gmail.com>
2025-08-19 15:59:33 +0900

    feat:  페이지 구조 셋팅

---
+++ src/app/register/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+export default function RegisterPage() {
+  return (
+    <>
+      <div>회원가입페이지</div>
+      <div>
+        회원
+        <div>가입</div>
+      </div>
+    </>
+  );
+}
