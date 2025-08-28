aa563711c452333e4f58800cee13864c47b5d460
mewmaze <aybin0108@gmail.com>
2025-08-28 11:26:17 +0900

    feat: 메인페이지, 새로운 여행 생성 페이지

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,41 @@
 'use client';
+import { useEffect, useState } from 'react';
 import LogoutButton from '../components/auth/LogoutButton';
 import { useAuth } from '../hooks/useAuth';
+import { TRIPS_PATH } from '../constants/routes';
+import { useRouter } from 'next/navigation';
+import type { Trip } from '../type/trip';
+import { createClient } from '../utils/supabase/client';
+import TripList from '../components/trip/TripList';
+import { NEW_TRIP_PATH } from '../constants/routes';
+
 export default function Home() {
   const { user } = useAuth();
+  const nickname = user?.user_metadata?.nickname;
+  const router = useRouter();
+  const [trips, setTrips] = useState<Trip[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!user) return;
+    const supabase = createClient();
+
+    supabase
+      .from('trips')
+      .select('*')
+      .eq('user_id', user.id)
+      .then(({ data, error }) => {
+        if (data) setTrips(data);
+        setLoading(false);
+      });
+  }, [user]);
+
+  useEffect(() => {
+    if (!user || loading) return;
+    if (trips.length === 0) {
+      router.push(NEW_TRIP_PATH);
+    }
+  }, [user, trips, router]);
 
   if (!user) {
     return (
@@ -12,9 +45,5 @@ export default function Home() {
       </>
     );
   }
-  return (
-    <>
-      <h1>환영합니다, {user.email}님!</h1> <LogoutButton />
-    </>
-  );
+  return <>{trips.length > 0 ? <TripList trips={trips} /> : null}</>;
 }
--- src/app/providers/providers.tsx
+++ src/app/providers/providers.tsx
@@ -3,11 +3,14 @@
 import { ThemeProvider } from 'next-themes';
 import { ReactNode } from 'react';
 import { AuthProvider } from '../../context/AuthContext';
+import { CountryProvider } from '../../context/CountryContext';
 
 export function Providers({ children }: { children: ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <AuthProvider>{children}</AuthProvider>
+      <AuthProvider>
+        <CountryProvider>{children}</CountryProvider>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
