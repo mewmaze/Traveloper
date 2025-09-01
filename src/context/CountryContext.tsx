2801227b6462192e743b1c2434784615cd14c7c2
mewmaze <aybin0108@gmail.com>
2025-09-01 15:42:31 +0900

    feat: 공공 API 연동하여 국가명 및 국기 정보 불러오도록 CountryContext 수정

--- src/context/CountryContext.tsx
+++ src/context/CountryContext.tsx
@@ -8,13 +8,23 @@ export function CountryProvider({ children }: { children: React.ReactNode }) {
   const [countries, setCountries] = useState<Country[]>([]);
 
   useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all?fields=name,cca2,flags')
+    fetch(
+      `https://apis.data.go.kr/1262000/CountryFlagService2/getCountryFlagList2?serviceKey=${process.env.NEXT_PUBLIC_FLAG_API_KEY}&returnType=JSON&numOfRows=999`,
+    )
       .then((res) => res.json())
       .then((data) => {
-        (data as Country[]).sort((a, b) => a.name.common.localeCompare(b.name.common));
-        setCountries(data as Country[]);
+        console.log('api응답:', data);
+        const items = Array.isArray(data?.response.body?.items?.item)
+          ? data.response.body.items.item
+          : [];
+        console.log('s', items);
+        const countries: Country[] = items.map((item: any) => ({
+          name: item.country_nm,
+          flag: item.download_url,
+          countryCode: item.country_iso_alp2,
+        }));
+        setCountries(countries);
       });
   }, []);
-
   return <CountryContext.Provider value={countries}>{children}</CountryContext.Provider>;
 }
