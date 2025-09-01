d2f4bf3bfc17f94b94efbfa874101455401a8f4a
mewmaze <aybin0108@gmail.com>
2025-09-01 15:52:28 +0900

    fix: 배포 any 타입 오류 수정

--- src/type/country.ts
+++ src/type/country.ts
@@ -3,3 +3,8 @@ export interface Country {
   flag: string;
   countryCode: string;
 }
+export interface CountryApiItem {
+  country_nm: string;
+  download_url: string;
+  country_iso_alp2: string;
+}
