478be303c6c4ab745f86cccffb04716a3878e3f8
mewmaze <aybin0108@gmail.com>
2025-09-17 15:02:26 +0900

    feature: 지출 목록 구현

---
+++ src/components/spend/SpendList.tsx
@@ -0,0 +1,29 @@
+'use server';
+import { createClient } from '../../utils/supabase/server';
+import SpendItem from './SpendItem';
+import type { SpendRecord } from '../../type/spend';
+
+export default async function SpendList({ tripId }: { tripId: string }) {
+  const supabase = await createClient();
+  const { data: spends, error } = await supabase
+    .from('spend_records')
+    .select('*')
+    .eq('trip_id', Number(tripId))
+    .order('created_at', { ascending: false });
+
+  if (error) return <div>지출 내역을 불러올 수 없습니다.</div>;
+  console.log('spends:', spends);
+  return (
+    <div>
+      <div className="flex px-3 py-2 bg-gray-50 items-center mb-2 font-bold">
+        <div className="flex-1 px-2 border-r border-gray-300">카드/현금</div>
+        <div className="flex-1 px-2 border-r border-gray-300">금액</div>
+        <div className="flex-1 px-2 border-r border-gray-300">유형</div>
+        <div className="flex-[3] px-2">메모</div>
+      </div>
+      {spends?.map((spend: SpendRecord) => (
+        <SpendItem key={spend.id} spend={spend} />
+      ))}
+    </div>
+  );
+}
