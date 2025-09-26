e4c41e49e5c1623b18abc9d65e533c81843163cf
mewmaze <aybin0108@gmail.com>
2025-09-26 15:16:25 +0900

    feature: useAuth에서 로딩값 제공

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -3,15 +3,21 @@ import { createContext, useEffect, useState } from 'react';
 import type { AuthContextType } from '../type/auth';
 import type { User } from '@supabase/supabase-js';
 import { createClient } from '../utils/supabase/client';
+import { set } from 'zod';
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const supabase = createClient();
-    supabase.auth.getUser().then(({ data }) => setUser(data.user));
+
+    supabase.auth.getUser().then(({ data }) => {
+      setUser(data.user);
+      setLoading(false);
+    });
 
     const {
       data: { subscription },
@@ -21,5 +27,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
 
     return () => subscription.unsubscribe();
   }, []);
-  return <AuthContext.Provider value={{ user, setUser }}>{children}</AuthContext.Provider>;
+
+  return <AuthContext.Provider value={{ user, setUser, loading }}>{children}</AuthContext.Provider>;
 }
