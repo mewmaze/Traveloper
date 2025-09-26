c5aaa82dda555c8c20ad9518049560bbc86b49c0
mewmaze <aybin0108@gmail.com>
2025-09-26 21:27:57 +0900

    feature: 로그아웃후 메인페이지로 이동

--- src/components/auth/LogoutButton.tsx
+++ src/components/auth/LogoutButton.tsx
@@ -1,13 +1,16 @@
+import { useRouter } from 'next/navigation';
 import { useAuth } from '../../hooks/useAuth';
 import { createClient } from '../../utils/supabase/client';
-
+import { ROOT_PATH } from '../../constants/routes';
 export default function LogoutButton() {
   const { setUser } = useAuth();
+  const router = useRouter();
   const supabase = createClient();
 
   async function handleLogout() {
     await supabase.auth.signOut();
     setUser(null);
+    router.push(ROOT_PATH);
   }
   return (
     <button
