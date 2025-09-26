dfe4cf666abdd1e3c9e73dc8948f324b6e231928
mewmaze <aybin0108@gmail.com>
2025-09-26 15:19:36 +0900

    feature: 로그인,회원가입 라우터 추가

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,3 +2,5 @@ export const ROOT_PATH = '/';
 export const TRIPS_PATH = '/trips';
 export const NEW_TRIP_PATH = '/trips/new';
 export const EXCHANGE_PATH = '/exchange';
+export const LOGIN_PATH = '/login';
+export const SIGNUP_PATH = '/register';
