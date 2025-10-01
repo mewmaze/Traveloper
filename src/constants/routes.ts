cc567a5095cba353f13e2eec863b5a478cd84f04
mewmaze <aybin0108@gmail.com>
2025-10-01 10:41:56 +0900

    feature: 통계 페이지 추가

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -4,3 +4,4 @@ export const NEW_TRIP_PATH = '/trips/new';
 export const EXCHANGE_PATH = '/exchange';
 export const LOGIN_PATH = '/login';
 export const SIGNUP_PATH = '/register';
+export const STATISTICS_PATH = '/spend-stats';
