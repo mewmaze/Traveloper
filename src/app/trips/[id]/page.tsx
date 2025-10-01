42f1c72753259db67c0462f9fc5c1d8a314d78f4
mewmaze <aybin0108@gmail.com>
2025-10-01 10:43:34 +0900

    feature: 통계, 환율추가 영역 추가

--- src/app/trips/[id]/page.tsx
+++ src/app/trips/[id]/page.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import SpendInputForm from '../../../components/form/SpendInputForm';
 import SpendList from '../../../components/spend/SpendList';
 import SpendSummary from '../../../components/spend/SpendSummary';
+import SpendActions from '../../../components/spend/SpendActions';
 import { getSpendByTrip } from '../../../actions/spends';
 
 interface TripDetailPageProps {
@@ -20,7 +21,10 @@ export default async function TripDetailPage({ params, searchParams }: TripDetai
 
   return (
     <div className="flex flex-col min-h-screen">
-      <SpendSummary tripId={id} />
+      <div className="flex justify-between items-center p-2 border-b">
+        <SpendSummary tripId={id} />
+        <SpendActions tripId={id} />
+      </div>
       <div className="flex-1 mt-2">
         <SpendList tripId={id} data={data} selectedDay={day} />
       </div>
